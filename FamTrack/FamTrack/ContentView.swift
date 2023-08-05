ca8f44a3eea60c38663150fdac58c6f5e4c9756f
Vian du Plessis <125647194+vian-duppie@users.noreply.github.com>
2023-08-05 11:51:14 +0200

    Initialised Application

---
+++ FamTrack/FamTrack/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FamTrack
+//
+//  Created by Vian du Plessis on 2023/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FamTrack/FamTrack/FamTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamTrackApp.swift
+//  FamTrack
+//
+//  Created by Vian du Plessis on 2023/08/05.
+//
+
+import SwiftUI
+
+@main
+struct FamTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
