4a28ae0763c9e8a3f5ae926af003a26023031271
Vian du Plessis <125647194+vian-duppie@users.noreply.github.com>
2023-08-08 10:47:47 +0200

    Created Custom Button

---
+++ FamTrack/FamTrack/Shared/Reusables/Components/CustomButton.swift
@@ -0,0 +1,34 @@
+//
+//  CustomButton.swift
+//  FamTrack
+//
+//  Created by Vian du Plessis on 2023/08/08.
+//
+
+import SwiftUI
+
+struct CustomButton: View {
+    var label: String = "label"
+    var clicked: () -> Void = {}
+    var expand: Bool = false
+    
+    var body: some View {
+        Button(action: clicked) {
+            Text(label)
+                .padding(.horizontal, 30)
+                .padding(.vertical, 10)
+                .foregroundColor(.white)
+                .font(Font.custom("Poppins-Medium", size: 15))
+        }
+        .frame(maxWidth: expand ? .infinity : 150)
+        .background(Color("SecondaryDarkBlue"))
+        .cornerRadius(10)
+        .lineLimit(1)
+    }
+}
+
+struct CustomButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomButton()
+    }
+}
