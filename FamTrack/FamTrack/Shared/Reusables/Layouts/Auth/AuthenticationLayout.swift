aee93d7199c77508f6b05aa91740599169e5c342
Vian du Plessis <125647194+vian-duppie@users.noreply.github.com>
2023-08-09 15:09:26 +0200

    Updated Authentication Layout

--- FamTrack/FamTrack/Shared/Reusables/Layouts/Auth/AuthenticationLayout.swift
+++ FamTrack/FamTrack/Shared/Reusables/Layouts/Auth/AuthenticationLayout.swift
@@ -13,75 +13,78 @@ struct AuthenticationLayout<Content: View>: View {
     var heading: String = "Heading"
     var subHeading: String = "Subheading"
     var mainAction: () -> Void = {}
-    var secondaryAction: () -> Void = {}
+    var secondaryAction: AnyView
     var lineButtonLabel: String = "Line Label"
     var lineButtonText: String = "Line Button Text"
     var lineButtonOpacity: Double = 1
     var buttonLabel: String = "Button"
     
     var body: some View {
-        VStack {
+        NavigationStack {
             VStack {
-                ZStack {
-                    Image("Shape")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(maxWidth: 100)
-                    
-                    Ellipse()
-                        .frame(maxWidth: 100, maxHeight: 100)
-                        .background(Color("WhiteBlur"))
-                        .opacity(0.5)
-                        .blur(radius: 120)
-                }
-            }
-            .frame(maxWidth: .infinity, alignment: .topTrailing)
-            
-            Spacer()
-            
-            VStack(spacing: 30) {
-                VStack(spacing: 5) {
-                    Text(heading)
-                        .foregroundColor(Color("SecondaryDarkBlue"))
-                        .font(Font.custom("Poppins-Medium", size: 32))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    Text(subHeading)
-                        .foregroundColor(Color("TextWhite"))
-                        .font(Font.custom("Poppins-Regular", size: 17))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
-                
                 VStack {
-                    formContent
+                    ZStack {
+                        Image("Shape")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: 100)
+                        
+                        Ellipse()
+                            .frame(maxWidth: 100, maxHeight: 100)
+                            .background(Color("WhiteBlur"))
+                            .opacity(0.5)
+                            .blur(radius: 120)
+                    }
                 }
+                .frame(maxWidth: .infinity, alignment: .topTrailing)
                 
-            }
-            .frame(maxWidth: .infinity)
-            .padding(.horizontal, 30)
-            
-            Spacer()
-            
-            VStack(spacing: 10) {
-                CustomButton(label: buttonLabel, clicked: mainAction)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
+                Spacer()
                 
-                HStack {
-                    Text(lineButtonLabel)
-                        .foregroundColor(.white)
-                        .font(Font.custom("Poppins-Light", size: 13))
-                        .opacity(lineButtonOpacity > 0 ? lineButtonOpacity : 1)
+                VStack(spacing: 30) {
+                    VStack(spacing: 5) {
+                        Text(heading)
+                            .foregroundColor(Color("SecondaryDarkBlue"))
+                            .font(Font.custom("Poppins-Medium", size: 32))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        Text(subHeading)
+                            .foregroundColor(Color("TextWhite"))
+                            .font(Font.custom("Poppins-Regular", size: 17))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
                     
-                    CustomLineButton(label: lineButtonText, clicked: secondaryAction, opacity: lineButtonOpacity)
-//                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    VStack {
+                        formContent
+                    }
+                    
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 30)
+                
+                Spacer()
+                
+                VStack(spacing: 10) {
+                    CustomButton(label: buttonLabel, clicked: mainAction)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    
+
+                    
+                    HStack {
+                        Text(lineButtonLabel)
+                            .foregroundColor(.white)
+                            .font(Font.custom("Poppins-Light", size: 13))
+                            .opacity(lineButtonOpacity > 0 ? lineButtonOpacity : 1)
+                        
+                        secondaryAction
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                .padding(.horizontal, 30)
                 .frame(maxWidth: .infinity, alignment: .trailing)
             }
-            .padding(.horizontal, 30)
-            .frame(maxWidth: .infinity, alignment: .trailing)
+            .padding(.vertical, 10)
+            .padding(.bottom, 30)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
-        .padding(.vertical, 10)
-        .padding(.bottom, 30)
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
