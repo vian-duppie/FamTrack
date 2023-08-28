4f84215b8f56ad4bef0c3f89b2211989b203898a
Vian du Plessis <125647194+vian-duppie@users.noreply.github.com>
2023-08-28 14:43:39 +0200

    Created Setup View

---
+++ FamTrack/FamTrack/Views/Screens/SetupView.swift
@@ -0,0 +1,41 @@
+//
+//  SetupView.swift
+//  FamTrack
+//
+//  Created by Vian du Plessis on 2023/08/10.
+//
+
+import SwiftUI
+
+struct SetupView: View {
+//    @ViewBuilder var ViewContent: Content
+    @StateObject var setupViewModel = SetupViewModel()
+//    @EnvironmentObject var userVM: UserStateViewModel
+    @State private var currentSetupView = 0
+    
+    
+    var views: [AnyView] = [
+        AnyView(SetupOneView()),
+        AnyView(SetupTwoView()),
+        AnyView(SetupThreeView())
+    ]
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            views[setupViewModel.currentView]
+                .environmentObject(setupViewModel)
+//                .environmentObject(userVM)
+        }
+        .padding(30)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .navigationBarHidden(true)
+    }
+}
+
+struct SetupView_Previews: PreviewProvider {
+    static var previews: some View {
+        SetupView()
+    }
+}
