b23a3437db78d38c2d98f3dd043a8fd002fa0569
Vian du Plessis <125647194+vian-duppie@users.noreply.github.com>
2023-09-02 14:55:10 +0200

    Updated User State View Model
    Added most functianality to fetch users in groups and fetch groups of current users

--- FamTrack/FamTrack/View Models/UserStateViewModel.swift
+++ FamTrack/FamTrack/View Models/UserStateViewModel.swift
@@ -12,37 +12,61 @@ import FirebaseFirestore
 @MainActor
 class UserStateViewModel: ObservableObject {
     private let db = Firestore.firestore()
+    
     @Published var showLogin = true
     @Published var isLoggedIn = false
     @Published var isBusy = false
     @Published var userDetails: User
     @Published var username = ""
+    @Published var userId = ""
+    
+    @Published var selectedGroup: Group = Group(name: "", inviteCode: "")
+    @Published var selectedGroupUsers: [User] = []
+    var listenerHandles: [ListenerRegistration] = []
+    
+    @Published var userGroups: [Group] = []
+    @Published var options: [DropdownOption] = []
     
     init() {
         userDetails = User()
         checkAuth()
     }
     
+    func reset() {
+        showLogin = true
+        isLoggedIn = false
+        isBusy = false
+        userDetails = User()
+        username = ""
+        userId = ""
+        selectedGroup = Group(name: "", inviteCode: "")
+        selectedGroupUsers = []
+        unsubscribeListeners()
+        userGroups = []
+        options = []
+    }
+    
+    func unsubscribeListeners() {
+        listenerHandles.forEach { $0.remove() }
+        listenerHandles.removeAll()
+    }
+    
     func getUserDetails() {
         let userId = getUserId()
-
+        
         db.collection("users").document(userId).getDocument { [weak self] document, error in
             guard let self = self else { return }
-
+            
             if let document = document, document.exists {
-                if let username = document.data()?["username"] as? String {
-                    DispatchQueue.main.async { // Make sure to update UI on the main thread
-                        self.userDetails.username = username
-                        self.username = username
-                    }
-//                    print(document.data())
-//                    print("This is your username \(self.username)")
-//                    print(userDetails?.username)
+                if let userData = try? document.data(as: User.self) {
+                    self.userDetails = userData
+                    print("The user data should have decoded")
+                } else {
+                    print(error?.localizedDescription ?? "Problem with decoding document")
                 }
             }
         }
     }
-
     
     func getUserId() -> String {
         return Auth.auth().currentUser?.uid ?? "No user id found"
@@ -52,7 +76,9 @@ class UserStateViewModel: ObservableObject {
         if Auth.auth().currentUser?.uid != nil {
             getUserDetails()
             isLoggedIn = true
+            print("The user is logged in")
         } else {
+            print("The user is not logged in")
             isLoggedIn = false
         }
     }
@@ -90,7 +116,15 @@ class UserStateViewModel: ObservableObject {
             .document(userId)
             .setData([
                 "username": username,
-                "email": email
+                "email": email,
+                "latitude": 0,
+                "longitude": 0,
+                "time": Date(),
+                "isDriving": false,
+                "speed": 0,
+//                "topSpeeds": [],
+                "totalDistanceDriven": 0
+                
             ]) { err in
                 if let err = err {
                     print("There was an error writing the document: \(err)")
@@ -126,10 +160,76 @@ class UserStateViewModel: ObservableObject {
     func signOut() async {
         do {
             try Auth.auth().signOut()
-            
             isLoggedIn = false
+            
+            reset()
         } catch let signOutError as NSError {
-          print("Error signing out: %@", signOutError)
+            print("Error signing out: %@", signOutError)
+        }
+    }
+    
+
+    
+    func getUserGroups() async {
+        let groupRef = db.collection("Groups")
+        
+        print("Your groups will be fetched now")
+        
+        do {
+            let querySnapshot = try await groupRef.whereField("members", arrayContains: getUserId()).getDocuments()
+            print("***************************** The groups are being fetched *****************************")
+            print("***************** \(getUserId())**********************")
+            for document in querySnapshot.documents {
+                if let group = try? document.data(as: Group.self) {
+                    self.userGroups.append(group)
+                    print("Document ID: \(document.documentID) => Data: \(document.data())")
+                    print("Hey you sexy man this is all your groups")
+                    
+                    let groupName = group.name
+                    
+                    // Append to dropdown options
+                    self.options.append(DropdownOption(key: group, val: groupName))
+                }
+            }
+        } catch {
+            print("Error getting documents: \(error)")
+        }
+    }
+    
+    
+
+    
+    func fetchUsersInGroup(group: Group) {
+        // Remove all Listeners
+        listenerHandles.forEach{$0.remove()}
+        listenerHandles.removeAll()
+        
+        selectedGroupUsers = []
+        userGroups = []
+        
+        let userRef = db.collection("users")
+        
+        guard let memberIds = group.members else {
+            return
+        }
+        
+        for id in memberIds {
+            let listener = userRef.document(id).addSnapshotListener { docSnapshot, err in
+                guard let doc = docSnapshot else {
+                    print("Error fetching document: \(err!)")
+                    return
+                }
+                
+                if let user = try? doc.data(as: User.self) {
+                    if let index = self.selectedGroupUsers.firstIndex(where: { $0.id == user.id }) {
+                        self.selectedGroupUsers[index] = user
+                    } else {
+                        self.selectedGroupUsers.append(user)
+                    }
+                }
+            }
+            
+            listenerHandles.append(listener)
         }
     }
 }
