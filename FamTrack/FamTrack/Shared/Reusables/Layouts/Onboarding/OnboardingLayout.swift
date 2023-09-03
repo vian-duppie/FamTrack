5d889d52f8699c970fa530c53e1a847706650687
Vian du Plessis <125647194+vian-duppie@users.noreply.github.com>
2023-09-03 03:07:56 +0200

    Cleaned Up Code

--- FamTrack/FamTrack/Shared/Reusables/Layouts/Onboarding/OnboardingLayout.swift
+++ FamTrack/FamTrack/Shared/Reusables/Layouts/Onboarding/OnboardingLayout.swift
@@ -16,43 +16,37 @@ struct OnboardingLayout: View {
     var body: some View {
         ZStack {
             Image(systemName: image)
-                 .resizable()
-                 .scaledToFit()
-                 .frame(maxWidth: 150, maxHeight: 100)
-             
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 150, maxHeight: 100)
+            
             Ellipse()
                 .frame(maxWidth: 150, maxHeight: 150)
                 .background(Color("WhiteBlur"))
                 .opacity(0.07)
                 .blur(radius: 40)
-         }
-         
-         Spacer()
-             .frame(height: 30)
-         
-         VStack(spacing: 30) {
-             Text(heading)
-                 .foregroundColor(Color("SecondaryDarkBlue"))
-                 .font(Font.custom("Poppins-Medium", size: 32))
-                 .frame(maxWidth: .infinity, alignment: .leading)
-             
-             VStack(spacing: 10) {
-                 Text(descriptionOne)
-                     .foregroundColor(Color("TextWhite"))
-                     .font(Font.custom("Poppins-Regular", size: 17))
-                     .frame(maxWidth: .infinity, alignment: .leading)
-                 
-                 Text(descriptionTwo)
-                     .foregroundColor(Color("TextWhite"))
-                     .font(Font.custom("Poppins-Regular", size: 17))
-                     .frame(maxWidth: .infinity, alignment: .leading)
-             }
-         }
-    }
-}
-
-struct OnboardingLayout_Previews: PreviewProvider {
-    static var previews: some View {
-        OnboardingLayout()
+        }
+        
+        Spacer()
+            .frame(height: 30)
+        
+        VStack(spacing: 30) {
+            Text(heading)
+                .foregroundColor(Color("SecondaryDarkBlue"))
+                .font(Font.custom("Poppins-Medium", size: 32))
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack(spacing: 10) {
+                Text(descriptionOne)
+                    .foregroundColor(Color("TextWhite"))
+                    .font(Font.custom("Poppins-Regular", size: 17))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text(descriptionTwo)
+                    .foregroundColor(Color("TextWhite"))
+                    .font(Font.custom("Poppins-Regular", size: 17))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
     }
 }
