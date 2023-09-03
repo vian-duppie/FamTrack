d45fd1da7f411220314e158cffad47e39f92f48f
Vian du Plessis <125647194+vian-duppie@users.noreply.github.com>
2023-09-03 03:03:54 +0200

    Update Driver Report View
    Showcase user driving data

--- FamTrack/FamTrack/Views/Screens/Profile/DriveReportView.swift
+++ FamTrack/FamTrack/Views/Screens/Profile/DriveReportView.swift
@@ -9,19 +9,7 @@ import Charts
 import SwiftUI
 
 struct DriveReportView: View {
-    let currentWeek: [StepCount] = [
-        StepCount(day: "20220717", steps: 5),
-        StepCount(day: "20220718", steps: 0),
-        StepCount(day: "20220719", steps: 2),
-        StepCount(day: "20220720", steps: 0),
-        StepCount(day: "20220721", steps: 2),
-        StepCount(day: "20220722", steps: 1),
-        StepCount(day: "20220723", steps: 5)
-    ]
-    
-    @State var showHighSpeed: Bool = false
-    @State var showChart: Bool = false
-    @State var test: Bool = false
+    var userData: User
     
     var body: some View {
         ScrollView {
@@ -45,7 +33,7 @@ struct DriveReportView: View {
                     .font(Font.custom("Poppins-Medium", size: 32))
                     .frame(maxWidth: .infinity, alignment: .leading)
                 
-                Text("You can view Anton's driving summary below")
+                Text("You can view \(userData.username)'s driving summary below")
                     .foregroundColor(Color("TextWhite"))
                     .font(Font.custom("Poppins-Regular", size: 17))
                     .frame(maxWidth: .infinity, alignment: .leading)
@@ -56,7 +44,7 @@ struct DriveReportView: View {
             
             HStack {
                 VStack(alignment: .leading, spacing: 5) {
-                    Text("200")
+                    Text(String(format: "%.1f", userData.totalDistanceDriven/1000))
                         .foregroundColor(Color("SecondaryDarkBlue"))
                         .font(Font.custom("Poppins-Medium", size: 20))
                         .frame(alignment: .leading)
@@ -78,7 +66,7 @@ struct DriveReportView: View {
                 
                 VStack(alignment: .leading, spacing: 5) {
                     HStack {
-                        Text("120")
+                        Text(String(userData.topSpeeds.last?.speed ?? 0))
                             .foregroundColor(Color("SecondaryDarkBlue"))
                             .font(Font.custom("Poppins-Medium", size: 20))
                             .frame(alignment: .leading)
@@ -88,7 +76,7 @@ struct DriveReportView: View {
                             .font(Font.custom("Poppins-Medium", size: 12))
                             .frame(alignment: .leading)
                     }
-              
+                    
                     
                     Spacer()
                     
@@ -106,7 +94,7 @@ struct DriveReportView: View {
                 Spacer()
                 
                 VStack(alignment: .leading, spacing: 5) {
-                    Text("3")
+                    Text(String(userData.driveCount))
                         .foregroundColor(Color("SecondaryDarkBlue"))
                         .font(Font.custom("Poppins-Medium", size: 20))
                         .frame(alignment: .leading)
@@ -130,13 +118,7 @@ struct DriveReportView: View {
                 .frame(height: 25)
             
             VStack {
-                AccordionChartItem(currentWeek: currentWeek)
-                
-                AccordionChartItem(currentWeek: currentWeek)
-                
-                AccordionChartItem(currentWeek: currentWeek)
-                
-                AccordionChartItem(currentWeek: currentWeek)
+                AccordionChartItem(topSpeedData: userData.topSpeeds)
             }
         }
         .padding(.horizontal, 30)
@@ -148,7 +130,7 @@ struct DriveReportView: View {
 
 struct AccordionChartItem: View {
     @State var showChart: Bool = false
-    var currentWeek: [StepCount]
+    var topSpeedData: [TopSpeedEntry] // Pass the topSpeedData
     
     var body: some View {
         VStack {
@@ -156,10 +138,9 @@ struct AccordionChartItem: View {
                 withAnimation {
                     showChart.toggle()
                 }
-            }
-            ){
+            }) {
                 HStack {
-                    Text("Phone Usage")
+                    Text("Top Speeds") // Modify the title
                         .foregroundColor(Color("TextWhite"))
                         .font(Font.custom("Poppins-Regular", size: 17))
                         .frame(alignment: .leading)
@@ -177,74 +158,55 @@ struct AccordionChartItem: View {
             
             if showChart {
                 VStack {
-                    Text("10")
-                        .foregroundColor(Color("Red"))
-                        .font(Font.custom("Poppins-SemiBold", size: 17))
-                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    if topSpeedData.count > 0 {
+                        Text(String(topSpeedData.count))
+                            .foregroundColor(Color("Red"))
+                            .font(Font.custom("Poppins-SemiBold", size: 17))
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
                     
                     Spacer()
                         .frame(height: 5)
                     
-                    Chart(currentWeek) {
-                        let step = Float($0.steps)
-                        BarMark(
-                            x: .value("Week Day", $0.weekday, unit: .day),
-                            y: .value("Step Count", $0.steps)
-                        )
-                        .cornerRadius(10)
-                        .interpolationMethod(.cardinal)
-                        .foregroundStyle(Color("Red"))
-                        .annotation(position: .automatic, alignment: .topLeading, spacing: 3) {
-                            Text("\(step, specifier: "%.0F")")
-                                .font(.footnote)
-                                .foregroundColor(.white)
+                    // Modify the chart to use topSpeedData
+                    if topSpeedData.count > 0 {
+                        Chart(topSpeedData) {
+                            let speed = Float($0.speed)
+                            BarMark(
+                                x: .value("Date", $0.date, unit: .day), // Display date
+                                y: .value("Speed", $0.speed)
+                            )
+                            .cornerRadius(10)
+                            .interpolationMethod(.cardinal)
+                            .foregroundStyle(Color("Red"))
+                            .annotation(position: .automatic, alignment: .topLeading, spacing: 3) {
+                                Text("\(speed, specifier: "%.0F")")
+                                    .font(.footnote)
+                                    .foregroundColor(.white)
+                            }
                         }
-                    }
-                    .chartYAxis(.hidden)
-                    .chartXAxis {
-                        AxisMarks (values: .stride (by: .day)) { value in
-                            AxisGridLine().foregroundStyle(.orange)
-                            AxisValueLabel(format: .dateTime.weekday(),
-                                           centered: true)
+                        .chartYAxis(.hidden)
+                        .chartXAxis {
+                            AxisMarks(values: .automatic) { value in
+                                AxisGridLine().foregroundStyle(.orange)
+                                AxisValueLabel(format: .dateTime.weekday(), centered: true)
+                            }
                         }
+                    } else {
+                        Text("There is no speed data")
+                            .foregroundColor(Color("Red"))
+                            .font(Font.custom("Poppins-SemiBold", size: 17))
+                            .frame(maxWidth: .infinity, alignment: .center)
                     }
+                    
                 }
                 .frame(maxWidth: .infinity)
                 .transition(.slide)
                 
                 Spacer()
                     .frame(minHeight: 25)
-
             }
         }
     }
 }
 
-struct DriveReportView_Previews: PreviewProvider {
-    static var previews: some View {
-        DriveReportView()
-    }
-}
-
-struct StepCount: Identifiable {
-    let id = UUID()
-    let weekday: Date
-    let steps: Int
-    
-    init(day: String, steps: Int) {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyyMMdd"
-        
-        self.weekday = formatter.date(from: day) ?? Date.distantPast
-        self.steps = steps
-    }
-    
-    var weekdayString: String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyyMMdd"
-        dateFormatter.dateStyle = .long
-        dateFormatter.timeStyle = .none
-        dateFormatter.locale = Locale(identifier: "en_US")
-        return dateFormatter.string(from: weekday)
-    }
-}
